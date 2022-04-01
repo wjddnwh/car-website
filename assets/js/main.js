c7bb145ac73a02ed4455fe72fc54e5ac7c5a6b93
JungWoo <wjddnwh12@naver.com>
2022-04-01 19:52:01 +0900

    finish response

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,6 +38,7 @@ function scrollHeader() {
 }
 
 window.addEventListener('scroll', scrollHeader)
+
 /*=============== POPULAR SWIPER ===============*/
 let swiperPopular = new Swiper(".popular__container", {
   loop: true,
@@ -80,10 +81,36 @@ function activeFeatured() {
 linkFeatured.forEach(l=> l.addEventListener('click', activeFeatured))
 
 /*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+  //When the scroll is higher than 350 viewport, and the show-scroll calss to the a tag with the scroll-text
+  if(this.scrollY >= 350) scrollUp.classList.add('show-scroll')
+  else scrollUp.classList.remove('show-scroll')
+}
 
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive() {
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }
+    else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
